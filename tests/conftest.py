77601a6155d8e626824f450dfd6a0aff6188e438
Ramiro Tician <ramirotician@gmail.com>
2020-08-31 11:32:42 -0300

    Python35 and lower needs a OrderdDict to ensure the insertion order.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,63 +1,53 @@
+from collections import OrderedDict
+
 import pytest
+
+
+def _create_dict_header(root):
+    d = OrderedDict()
+    d[root] = OrderedDict()
+    d[root]['@ns'] = 'soapenv'
+    d[root]['@attrs'] = OrderedDict()
+    d[root]['@attrs']['xmlns:soapenv'] = 'http://schemas.xmlsoap.org/soap/envelope/'
+    return d
 
 
 @pytest.fixture
 def simple_dict():
-    return {
-        'RootTag': {
-            '@ns': 'soapenv',
-            '@attrs': {
-                'xmlns:soapenv': 'http://schemas.xmlsoap.org/soap/envelope/'
-            },
-            'childTag': {
-                '@attrs': {
-                    'someAttribute': 'colors are nice'
-                },
-                'grandchild': 'This is a text tag'
-            }
-        }
-    }
+    d = _create_dict_header('RootTag')
+    d['RootTag']['childTag'] = OrderedDict()
+    d['RootTag']['childTag']['@attrs'] = OrderedDict()
+    d['RootTag']['childTag']['@attrs']['someAttribute'] = 'colors are nice'
+    d['RootTag']['childTag']['grandchild'] = 'This is a text tag'
+
+    return d
 
 
 @pytest.fixture
 def multi_type_dict():
-    return {
-        'Root': {
-            '@ns': 'soapenv',
-            '@attrs': {
-                'xmlns:soapenv': 'http://schemas.xmlsoap.org/soap/envelope/'
-            },
-            'Header': '',
-            'Body': {
-                'tag1': [1, 2, 3],
-                'tag2': {
-                    'child1': ' '
-                },
-                'tag3': 1.0,
-                'tag4': {1, 2, 3},
-                'tag5': False,
-                'tag6': tuple(),
-                'tag7': None
-            }
-        }
-    }
+    d = _create_dict_header('Root')
+    d['Root']['Header'] = ''
+    d['Root']['Body'] = OrderedDict()
+    d['Root']['Body']['tag1'] = [1, 2, 3]
+    d['Root']['Body']['tag2'] = OrderedDict()
+    d['Root']['Body']['tag2']['child1'] = ' '
+    d['Root']['Body']['tag3'] = 1.0
+    d['Root']['Body']['tag4'] = {1, 2, 3}
+    d['Root']['Body']['tag5'] = False
+    d['Root']['Body']['tag6'] = tuple()
+    d['Root']['Body']['tag7'] = None
+
+    return d
 
 
 @pytest.fixture
 def multi_attr_dict():
-    return {
-        'Envelope': {
-            '@ns': 'soapenv',
-            '@attrs': {
-                'xmlns:soapenv': 'http://schemas.xmlsoap.org/soap/envelope/'
-            },
-            'Header': '',
-            'Body': {
-                'tag1': {
-                    '@name': 'first_tag',
-                    '@value': 'foo bar'
-                },
-                'tag2': 'some value'
-            }
-        }
-    }
+    d = _create_dict_header('Envelope')
+    d['Envelope']['Header'] = ''
+    d['Envelope']['Body'] = OrderedDict()
+    d['Envelope']['Body']['tag1'] = OrderedDict()
+    d['Envelope']['Body']['tag1']['@name'] = 'first_tag'
+    d['Envelope']['Body']['tag1']['@value'] = 'foo bar'
+    d['Envelope']['Body']['tag2'] = 'some value'
+
+    return d
